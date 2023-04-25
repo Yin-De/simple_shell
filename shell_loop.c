1ee07692b06bb0821ee04c2464001dcf2d228951
Yin-De <111071078+Yin-De@users.noreply.github.com>
2023-04-25 13:45:01 +0100

    Update shell_loop.c

--- shell_loop.c
+++ shell_loop.c
@@ -1,12 +1,4 @@
 #include "shell.h"
-
-/**
- * hsh - main shell loop
- * @info: the parameter & return info struct
- * @av: the argument vector from main()
- *
- * Return: 0 on success, 1 on error, or error code
- */
 int hsh(info_t *info, char **av)
 {
 	ssize_t r = 0;
@@ -42,16 +34,6 @@ int hsh(info_t *info, char **av)
 	}
 	return (builtin_ret);
 }
-
-/**
- * find_builtin - finds a builtin command
- * @info: the parameter & return info struct
- *
- * Return: -1 if builtin not found,
- *			0 if builtin executed successfully,
- *			1 if builtin found but not successful,
- *			-2 if builtin signals exit()
- */
 int find_builtin(info_t *info)
 {
 	int i, built_in_ret = -1;
@@ -76,13 +58,6 @@ int find_builtin(info_t *info)
 		}
 	return (built_in_ret);
 }
-
-/**
- * find_cmd - finds a command in PATH
- * @info: the parameter & return info struct
- *
- * Return: void
- */
 void find_cmd(info_t *info)
 {
 	char *path = NULL;
@@ -118,13 +93,6 @@ void find_cmd(info_t *info)
 		}
 	}
 }
-
-/**
- * fork_cmd - forks a an exec thread to run cmd
- * @info: the parameter & return info struct
- *
- * Return: void
- */
 void fork_cmd(info_t *info)
 {
 	pid_t child_pid;
