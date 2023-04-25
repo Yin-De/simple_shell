400fcb7ab1b575f97dbff89e144f816d4424623f
Yin-De <111071078+Yin-De@users.noreply.github.com>
2023-04-25 13:46:16 +0100

    Update string1.c

--- string1.c
+++ string1.c
@@ -1,12 +1,4 @@
 #include "shell.h"
-
-/**
- * _strcpy - copies a string
- * @dest: the destination
- * @src: the source
- *
- * Return: pointer to destination
- */
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
@@ -21,13 +13,6 @@ char *_strcpy(char *dest, char *src)
 	dest[i] = 0;
 	return (dest);
 }
-
-/**
- * _strdup - duplicates a string
- * @str: the string to duplicate
- *
- * Return: pointer to the duplicated string
- */
 char *_strdup(const char *str)
 {
 	int length = 0;
@@ -44,13 +29,6 @@ char *_strdup(const char *str)
 		ret[length] = *--str;
 	return (ret);
 }
-
-/**
- *_puts - prints an input string
- *@str: the string to be printed
- *
- * Return: Nothing
- */
 void _puts(char *str)
 {
 	int i = 0;
@@ -63,14 +41,6 @@ void _puts(char *str)
 		i++;
 	}
 }
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
 int _putchar(char c)
 {
 	static int i;
